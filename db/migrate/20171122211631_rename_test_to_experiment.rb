f5f4aa7f503e4e8fecae7f7cafc6ba78af236e3c
Antonina Lebedeva <antonina.lebedeva@eman.cz>
2017-12-08 18:19:03 +0100

    uncomment some code

--- db/migrate/20171122211631_rename_test_to_experiment.rb
+++ db/migrate/20171122211631_rename_test_to_experiment.rb
@@ -1,11 +1,11 @@
 class RenameTestToExperiment < ActiveRecord::Migration[5.1]
   def change
-    # rename_table :tests, :experiments
-    # rename_table :test_parts, :experiment_parts
-    # rename_table :test_variables, :experiment_variables
+    rename_table :tests, :experiments
+    rename_table :test_parts, :experiment_parts
+    rename_table :test_variables, :experiment_variables
     rename_table :test_data, :experiment_data
 
-    # rename_column :experiment_parts, :test_id, :experiment_id
-    # rename_column :experiment_json_data, :test_id, :experiment_id
+    rename_column :experiment_parts, :test_id, :experiment_id
+    rename_column :experiment_json_data, :test_id, :experiment_id
   end
 end
